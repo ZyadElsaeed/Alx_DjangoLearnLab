f496e6a33369fa930052cca397bcd68affa3025f
ZyadElsaeed <zyad@example.com>
2026-02-15 08:00:40 -0800

    Correct project structure: django_blog/django_blog/settings.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_blog_app project.
+URL configuration for django_blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
