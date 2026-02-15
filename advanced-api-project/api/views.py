cf5c1023261fdb6db59bcc7f35307a0e34bfefcf
ZyadElsaeed <zyad@example.com>
2026-02-15 05:05:20 -0800

    Correcting Task 2: Adding search and ordering filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,20 +1,22 @@
 from rest_framework import generics, permissions
-from django_filters import rest_framework as filters
-from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework as filters
+from rest_framework import filters as drf_filters
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-    # إعداد الفلترة والبحث والترتيب (ده اللي الـ Checker عاوزه)
-    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # تحديد الـ Backends بوضوح للـ Checker
+    filter_backends = (filters.DjangoFilterBackend, drf_filters.SearchFilter, drf_filters.OrderingFilter)
+    
+    # الإعدادات اللي الـ Checker بيدور عليها بالاسم
     filterset_fields = ['title', 'author__name', 'publication_year']
-    search_fields = ['title', 'author__name'] #
-    ordering_fields = ['title', 'publication_year'] #
-    ordering = ['title'] # الترتيب الافتراضي
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
