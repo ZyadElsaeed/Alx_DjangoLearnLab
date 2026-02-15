f3b62ee2edd4fb906536f05423e333faa2d1607b
ZyadElsaeed <zyad@example.com>
2026-02-15 09:05:38 -0800

    Finalize Task 1: Authentication URLs and Redirects

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
