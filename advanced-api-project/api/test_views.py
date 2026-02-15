f0dcfd7caaeec5b35f41c92942d302efbd2307a4
ZyadElsaeed <zyad@example.com>
2026-02-15 06:37:51 -0800

    Final fix for Unit Tests: Covered CRUD and status codes

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -6,7 +6,7 @@ from .models import Book, Author
 
 class BookAPITestCase(APITestCase):
     def setUp(self):
-        # إنشاء مستخدم للاختبارات التي تتطلب صلاحيات
+        # إنشاء مستخدم ومؤلف وكتاب للاختبار
         self.user = User.objects.create_user(username='testuser', password='password123')
         self.author = Author.objects.create(name="Test Author")
         self.book = Book.objects.create(
@@ -14,27 +14,37 @@ class BookAPITestCase(APITestCase):
             publication_year=2020,
             author=self.author
         )
+        # تعريف الروابط
         self.list_url = reverse('book-list')
         self.create_url = reverse('book-create')
+        self.detail_url = reverse('book-detail', kwargs={'pk': self.book.pk})
+        self.update_url = reverse('book-update', kwargs={'pk': self.book.pk})
+        self.delete_url = reverse('book-delete', kwargs={'pk': self.book.pk})
 
-    def test_get_books_status_code(self):
-        # اختبار قراءة الكتب (يجب أن يعيد 200)
+    def test_get_books(self):
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_book_authenticated(self):
-        # تسجيل الدخول واختبار إنشاء كتاب (يجب أن يعيد 201)
         self.client.login(username='testuser', password='password123')
-        data = {
-            "title": "New Book",
-            "publication_year": 2024,
-            "author": self.author.id
-        }
+        data = {"title": "New Book", "publication_year": 2024, "author": self.author.id}
         response = self.client.post(self.create_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_book_unauthenticated(self):
-        # اختبار الإنشاء بدون تسجيل دخول (يجب أن يعيد 403 بسبب الصلاحيات)
-        data = {"title": "Ghost Book", "publication_year": 2024, "author": self.author.id}
-        response = self.client.post(self.create_url, data)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_update_book_authenticated(self):
+        self.client.login(username='testuser', password='password123')
+        data = {"title": "Updated Title", "publication_year": 2021, "author": self.author.id}
+        response = self.client.put(self.update_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, "Updated Title")
+
+    def test_delete_book_authenticated(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.delete(self.delete_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_filter_books_by_title(self):
+        response = self.client.get(f"{self.list_url}?title=Test Book")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
