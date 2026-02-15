f787dc2fb6b43c5627125f87cb040ef2e930a9e4
ZyadElsaeed <zyad@example.com>
2026-02-15 13:50:46 -0800

    Complete Task 2: Full CRUD system

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post-list')
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+# الـ Views القديمة لضمان استمرار Task 1
 from .forms import CustomUserCreationForm
 def register(request):
     if request.method == 'POST':
@@ -7,9 +43,10 @@ def register(request):
         if form.is_valid():
             form.save()
             return redirect('login')
-    else:
-        form = CustomUserCreationForm()
+    else: form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
+
+from django.contrib.auth.decorators import login_required
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html')
