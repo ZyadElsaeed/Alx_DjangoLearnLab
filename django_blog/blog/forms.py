cb9c144b7b6f309f85e18058e045f3c006116eab
Zyad Elsaeed <zyadelsaied@example.com>
2026-02-21 07:20:40 -0800

    Final Fix Task 4: Unified build with TagWidget and Search

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,15 +1,22 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ('email',)
+from taggit.forms import TagWidget
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+from django.contrib.auth.forms import UserCreationForm
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from django.db.models import Q
 from .models import Post, Comment
-from .forms import CustomUserCreationForm, PostForm, CommentForm
+from .forms import PostForm, CommentForm, CustomUserCreationForm
 
 class PostListView(ListView):
     model = Post
@@ -31,26 +31,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     success_url = reverse_lazy('post-list')
     def test_func(self): return self.request.user == self.get_object().author
 
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
-        return super().form_valid(form)
-    def get_success_url(self): return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
-
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Comment
-    form_class = CommentForm
-    def test_func(self): return self.request.user == self.get_object().author
-    def get_success_url(self): return reverse_lazy('post-detail', kwargs={'pk': self.get_object().post.id})
-
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    def test_func(self): return self.request.user == self.get_object().author
-    def get_success_url(self): return reverse_lazy('post-detail', kwargs={'pk': self.get_object().post.id})
-
 def search_posts(request):
     query = request.GET.get('q')
     posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query)).distinct() if query else Post.objects.none()
@@ -60,7 +40,8 @@ class PostByTagListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    def get_queryset(self): return Post.objects.filter(tags__name__in=[self.kwargs.get('tag_slug')])
+    def get_queryset(self):
+        return Post.objects.filter(tags__name__in=[self.kwargs.get('tag_slug')])
 
 def register(request):
     if request.method == 'POST':
